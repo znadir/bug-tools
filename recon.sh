6357cc806d4fde7b1d31b9dca69b0a1b930d9a27
Nadir <115375151+znadir@users.noreply.github.com>
2024-12-19 23:20:37 -0500

    remove ml detection

--- recon.sh
+++ recon.sh
@@ -85,7 +85,7 @@ subzy run --targets subdomains.txt
 EOF
 
 run_tool "Httpx" "Filtering subdomains..." <<EOF
-cat subdomains.txt | httpx -fd -fc 403,401 -fl 0 -fep | tee subdomains.txt
+cat subdomains.txt | httpx -fd -fc 403,401 -fl 0 | tee subdomains.txt
 EOF
 
 if [[ ! -s subdomains.txt ]]; then
